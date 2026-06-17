56d58d4b09f38079103ea6ff9a5abaf490d4e3d4
Delgermurun Munkhbat <23B1NUM2871@stud.num.edu.mn>
2026-06-17 23:05:56 +0800

    Update firebase.ts

--- cedig---family-tree/src/lib/firebase.ts
+++ cedig---family-tree/src/lib/firebase.ts
@@ -33,7 +33,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log(firebaseConfig);
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
